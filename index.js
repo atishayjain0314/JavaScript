ae1bd9b37c725af393552d6a47a4392aa231100d
200110022atishay <atishayjain0314@gmail.com>
2023-03-20 03:04:42 +0530

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+// let a = prompt("what is your age");
+// let b = prompt("what is your age");
+// let c = prompt("what is old");
+
+// console.log(a+b+c);
+console.log("start");
+setTimeout(function(){
+    console.log("hey");
+},3000)
+console.log("end");
